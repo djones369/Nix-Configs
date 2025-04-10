49a5a49c1f3a98dee134ee70d4370fc865a3ab6f
Dave Jones <djones369@gmail.com>
2025-04-10 19:44:03 -0400

    Update configuration.nix
    Updated Hostname

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "Tenacious-Nix"; # Define your hostname.
+  networking.hostName = "TenaciousNix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.extraHosts =
     ''
